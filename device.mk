f4790f4b891a740ed121a18a0d8cba9218853f6b
madmax7896 <thegreatsagar123@gmail.com>
2021-02-25 22:18:38 +0530

    ginkgo: Enable updatable apex

--- device.mk
+++ device.mk
@@ -163,6 +163,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.3-service.clearkey
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.ginkgo
